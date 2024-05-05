47ad83eaea7790d1317095a7018d10ae1ff747f5
denis <deadkor26@gmail.com>
2024-05-05 11:36:10 +0300

    //

--- src/generics/3.ts
+++ src/generics/3.ts
@@ -1,12 +1,10 @@
+type FirstType = { name: string };
+
+type SecondType = { age: number };
+
 function merge<T extends object, U extends object>(objA: T, objB: U): T & U {
-return Object.assign({}, objA, objB);
+  return Object.assign(objA, objB);
 }
 
-const obj1 = { name: "John", age: 30 };
-const obj2 = { city: "New York", hobbies: ["reading", "music"] };
-
-const mergedObj = merge(obj1, obj2);
-
-console.log(mergedObj.name);
-console.log(mergedObj.city);
-console.log(mergedObj.hobbies);
+const merged = merge<FirstType, SecondType>({ name: "Maks" }, { age: 27 });
+console.log(merged);
