6404e58719ff912de31cc5801f1d3f9896695366
denis <deadkor26@gmail.com>
2024-05-05 11:12:12 +0300

    /

--- src/generics/3.ts
+++ src/generics/3.ts
@@ -1,4 +1,4 @@
-function merge<T, U>(objA: T, objB: U): T & U {
+function merge<T extends object, U extends object>(objA: T, objB: U): T & U {
 return Object.assign({}, objA, objB);
 }
 
@@ -7,6 +7,6 @@ const obj2 = { city: "New York", hobbies: ["reading", "music"] };
 
 const mergedObj = merge(obj1, obj2);
 
-console.log(mergedObj.name); 
-console.log(mergedObj.city); 
+console.log(mergedObj.name);
+console.log(mergedObj.city);
 console.log(mergedObj.hobbies);
